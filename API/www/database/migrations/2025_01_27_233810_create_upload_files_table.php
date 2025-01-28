515a669f44ca3da48ce1a6dee45cb95180e5be72
Messias Barreto <messiasg.barreto@gmail.com>
2025-01-27 21:02:18 -0300

    feat: adicionando model e migrate UploadFile

---
+++ API/www/database/migrations/2025_01_27_233810_create_upload_files_table.php
@@ -0,0 +1,79 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('upload_files', function (Blueprint $table) {
+            $table->id();
+            $table->string('RptDt')->nullable();
+            $table->string('TckrSymb')->nullable();
+            $table->string('Asst')->nullable();
+            $table->string('AsstDesc')->nullable();
+            $table->string('SgmtNm')->nullable();
+            $table->string('MktNm')->nullable();
+            $table->string('SctyCtgyNm')->nullable();
+            $table->string('XprtnDt')->nullable();
+            $table->string('XprtnCd')->nullable();;
+            $table->string('TradgStartDt')->nullable();
+            $table->string('TradgEndDt')->nullable();
+            $table->string('BaseCd')->nullable();
+            $table->string('ConvsCritNm')->nullable();
+            $table->string('MtrtyDtTrgtPt')->nullable();
+            $table->string('ReqrdConvsInd')->nullable();
+            $table->string('ISIN')->nullable();
+            $table->string('CFICd')->nullable();
+            $table->string('DlvryNtceStartDt')->nullable();
+            $table->string('DlvryNtceEndDt')->nullable();
+            $table->string('OptnTp')->nullable();
+            $table->integer('CtrctMltplr')->nullable();
+            $table->integer('AsstQtnQty')->nullable();
+            $table->integer('AllcnRndLot')->nullable();
+            $table->string('TradgCcy')->nullable();
+            $table->string('DlvryTpNm')->nullable();
+            $table->integer('WdrwlDays')->nullable();
+            $table->integer('WrkgDays')->nullable();
+            $table->integer('ClnrDays')->nullable();
+            $table->string('RlvrBasePricNm')->nullable();
+            $table->string('OpngFutrPosDay')->nullable();
+            $table->string('SdTpCd1')->nullable();
+            $table->string('UndrlygTckrSymb1')->nullable();
+            $table->string('SdTpCd2')->nullable();
+            $table->string('UndrlygTckrSymb2')->nullable();
+            $table->string('PureGoldWght')->nullable();
+            $table->string('ExrcPric')->nullable();
+            $table->string('OptnStyle')->nullable();
+            $table->string('ValTpNm')->nullable();
+            $table->boolean('PrmUpfrntInd')->nullable();
+            $table->string('OpngPosLmtDt')->nullable();
+            $table->string('DstrbtnId')->nullable();
+            $table->string('PricFctr')->nullable();
+            $table->string('DaysToSttlm')->nullable();
+            $table->string('SrsTpNm')->nullable();
+            $table->string('PrtcnFlg')->nullable();
+            $table->string('AutomtcExrcInd')->nullable();
+            $table->string('SpcfctnCd')->nullable();
+            $table->string('CrpnNm')->nullable();
+            $table->string('CorpActnStartDt')->nullable();
+            $table->string('CtdyTrtmntTpNm')->nullable();
+            $table->string('MktCptlstn')->nullable();
+            $table->string('CorpGovnLvlNm')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('upload_files');
+    }
+};
