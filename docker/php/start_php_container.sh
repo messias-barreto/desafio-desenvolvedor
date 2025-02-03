58d63ae075b6c4143d105f0ed3c2f9501760666c
Messias Barreto <messiasg.barreto@gmail.com>
2025-02-03 17:05:43 -0300

    refactor: refaotando script para adicionar a chave de autenticacao do passport

--- docker/php/start_php_container.sh
+++ docker/php/start_php_container.sh
@@ -5,6 +5,10 @@ if [ ! -d "/usr/share/nginx/html/vendor" ]; then
     cd /usr/share/nginx/html && composer install --no-dev
 fi
 
+if [ ! -f "/usr/share/nginx/html/storage/oauth-private.key" ]; then
+    cd /usr/share/nginx/html && php artisan passport:key
+fi
+
 # Agora que a pasta vendor está garantida, inicia o supervisord
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 
