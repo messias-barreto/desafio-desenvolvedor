59946764d0c2018c1a2ba5d14fdaade45fdaa609
Messias Barreto <messiasg.barreto@gmail.com>
2025-02-03 14:20:35 -0300

    refactor: refatorando o script para inicializacao do container

--- docker/php/start_php_container.sh
+++ docker/php/start_php_container.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-# Inicia o supervisord
+
+# Verifica se a pasta vendor existe. Se não, roda o composer install
+if [ ! -d "/usr/share/nginx/html/vendor" ]; then
+    cd /usr/share/nginx/html && composer install --no-dev
+fi
+
+# Agora que a pasta vendor está garantida, inicia o supervisord
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 
-# Inicia o PHP-FPM
+# Inicia o PHP-FPM (caso não seja iniciado pelo supervisor)
 php-fpm
